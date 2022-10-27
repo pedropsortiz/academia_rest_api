48fb45715cc9235475943a7c35899e411a1e85c5
Pedro Ortiz <pedrops.ortiz@gmail.com>
2022-10-27 15:00:20 -0300

    Criação do projeto de academia API Rest com o Spring Start

---
+++ src/main/java/br/ufsm/csi/poow2/academia_java/AcademiaJavaApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.poow2.academia_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/csi/poow2/academia_java/AcademiaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.poow2.academia_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
